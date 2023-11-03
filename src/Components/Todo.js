8041ba1a7209862ec155a736ea65593b795b935c
Aishwarya <9a9bhardwaj@gmail.com>
2023-11-04 04:53:24 +0530

    CSS changes

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import './Todo.css'
  class Todo extends Component {
 constructor(){
     super();
     this.state={
-        tasks:[{task:'Check Mails',id:1},{task:'Read article',id:2},{task:'complete hw',id:3}],
+        tasks:[{task:'Check Mails',id:1},{task:'Read article',id:2},{task:'Complete Homework',id:3}],
         currTask:''
     }
 }
@@ -36,16 +36,16 @@ handleDelete=(id)=>{
   render() {
     console.log('render')
     return (
-      <div>
-        <input type='text'  value={this.state.currTask} onChange={this.handleChange}/>
-        <button onClick={this.handleSubmit}>Submit</button>
+      <div class="wrapper">
+        <input type='text'  value={this.state.currTask} onChange={this.handleChange} class="textfield"/>
+        <button onClick={this.handleSubmit} class="submit">Submit</button>
         <ul>
 {
     this.state.tasks.map((taskObj)=>
     (
         <li key={taskObj.id}>
             <p>{taskObj.task}</p>
-            <button onClick={()=> this.handleDelete(taskObj.id)}>Delete</button> 
+            <button onClick={()=> this.handleDelete(taskObj.id)} class="delete">Delete</button> 
             {/* onclick delete button so we use arrow function as DOM will directly call delete but now we givr func defination  */}
         </li>
     )
